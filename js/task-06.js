1c707529ee9d043558f4eecf81c6f23518638192
IvanBogachov <24term@gmail.com>
2022-10-04 15:03:48 +0300

    task7 ok

---
+++ js/task-06.js
@@ -0,0 +1,17 @@
+const textInput = document.querySelector('#validation-input')
+
+textInput.addEventListener('blur', onBlurList)
+
+function onBlurList(event) { 
+if (event.target.value.length == textInput.getAttribute('data-length')) {
+		textInput.classList.add('valid')
+		if (textInput.classList.contains('invalid')) {
+			textInput.classList.remove('invalid')
+		}
+	} else {
+		if (textInput.classList.contains('valid')) {
+			textInput.classList.remove('valid')
+		}
+		textInput.classList.add('invalid')
+	}
+}
